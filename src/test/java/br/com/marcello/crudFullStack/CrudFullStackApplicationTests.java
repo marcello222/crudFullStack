efa141290899a88198f99042c9e394bd1a777df8
marcello222 <marcelloeduardo222@gmail.com>
2022-10-08 15:28:38 -0300

    Initial commit

---
+++ src/test/java/br/com/marcello/crudFullStack/CrudFullStackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcello.crudFullStack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudFullStackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
